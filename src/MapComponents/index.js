ac3182c17bdf2fcac086fd8715847a709d63f180
unknown <bipinmsit@aspecscire.com>
2021-06-23 15:24:11 +0530

    structuring the code

---
+++ src/MapComponents/index.js
@@ -0,0 +1,7 @@
+import MapComponents from "./MapComponents";
+import FeatureInfo from "./FeatureInfo";
+import Legend from "./Legend";
+import AddWmsLayers from "./AddWmsLayers";
+import "./mapComponents.css";
+
+export { MapComponents, FeatureInfo, Legend, AddWmsLayers };
